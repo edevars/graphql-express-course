5bac2250acbb526c95f760c397867d2fc866ceca
edevars <e.devars.dev@gmail.com>
2020-07-05 18:01:25 -0500

    :sparkles: Settinng schemas and resolvers in apart files and mocking data

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const { buildSchema } = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
-const expressPlayground = require('graphql-playground-middleware-express')
-  .default;
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const resolvers = require('./lib/resolvers');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 // Defining schema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-        goodbye: String
-    }
-`);
-
-// Setting resolvers
-const resolvers = {
-  hello() {
-    return 'Hello world!';
-  },
-  goodbye() {
-    return 'Goodbye cruel world!';
-  },
-};
+const pathToSchema = join(__dirname, 'lib', 'schema.graphql');
+const schema = buildSchema(readFileSync(pathToSchema, 'utf-8'));
 
 // Config of express-graphql
 app.use(
